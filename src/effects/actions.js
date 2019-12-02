03f408f504e963a012521cb135d90b7e6e51f143
Robert Biggs <rbiggs@ymail.com>
2019-12-02 08:11:06 -0800

    Reorg of actions.

--- src/effects/actions.js
+++ src/effects/actions.js
@@ -11,54 +11,49 @@ import {idb} from '@composi/idb'
  * @param {Message} msg
  * @param {Send} send
  */
-export function actions(state, msg, send) {
-
-  // Match received msg with tagged union types:
-  return match(msg, {
-
-    AddItem: () => {
-      if (state.inputValue) {
-        state.items.push({
-          key: state.newKey++,
-          value: state.inputValue
-        })
-        state.inputValue = ''
-        send(SaveLocally(state))
-      } else {
-        alert('Please provide a value before submitting.')
-      }
-      return state
-    },
-
-    DeleteItem: key => {
-      state.items = state.items.filter(item => item.key !== key)
-      send(SaveLocally(state))
-      return state
-    },
-
-    MakeDeletable: key => {
-      state.items = state.items.map(item => {
-        if (item.key === key) {
-          item.deletable = true
-          setTimeout(() => {
-            send(DeleteItem(key))
-          }, 1000)
-        }
-        return item
+export const actions = (state, msg, send) => match(msg, {
+  AddItem: () => {
+    if (state.inputValue) {
+      state.items.push({
+        key: state.newKey++,
+        value: state.inputValue
       })
-      return state
-    },
+      state.inputValue = ''
+      send(SaveLocally(state))
+    } else {
+      alert('Please provide a value before submitting.')
+    }
+    return state
+  },
 
-    SaveLocally: data => {
-      idb.set('app-state', data)
-      return state
-    },
+  DeleteItem: key => {
+    state.items = state.items.filter(item => item.key !== key)
+    send(SaveLocally(state))
+    return state
+  },
 
-    UpdateInputValue: value => {
-      state.inputValue = value
-      return state
-    },
+  MakeDeletable: key => {
+    state.items = state.items.map(item => {
+      if (item.key === key) {
+        item.deletable = true
+        setTimeout(() => {
+          send(DeleteItem(key))
+        }, 1000)
+      }
+      return item
+    })
+    return state
+  },
 
-    UseFetchedData: data => data
-  })
-}
+  SaveLocally: data => {
+    idb.set('app-state', data)
+    return state
+  },
+
+  UpdateInputValue: value => {
+    state.inputValue = value
+    return state
+  },
+
+  UseFetchedData: data => data
+})
